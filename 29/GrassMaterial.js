b16f1c6541abf16b80f051db2e8efb6973ded02a
Avaer Kazmer <a@modules.io>
2022-01-08 05:00:51 -0500

    Color checkpoint

--- 29/GrassMaterial.js
+++ 29/GrassMaterial.js
@@ -121,7 +121,7 @@ void main() {
   vec3 color4 = vec3(216., 255., 147.)/ 255.;
 
   vec3 color = mix(mix(color1, color2, vUv.y), color3, vDry);
-  color = mix(color, color4, vLight);
+  color = mix(color, color4, 0.3 + vLight*0.7);
   fragColor = vec4(color * vUv.y, 1.);
 }`;
 
